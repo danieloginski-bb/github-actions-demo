9f36e925d69ec1b023031469275940f461a8c3f2
Daniel Oginski <danielo@backbase.com>
2022-05-24 15:21:32 +0200

    bump major - some changes

--- module1/src/main/java/com/backbase/module1/Module1Application.java
+++ module1/src/main/java/com/backbase/module1/Module1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Module1Application {
 
   public static void main(String[] args) {
-    System.out.println("test1");
+    System.out.println("test1rwer");
     SpringApplication.run(Module1Application.class, args);
   }
 
